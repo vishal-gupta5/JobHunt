43ff8148ce87edb2658d7d79e0e5fbc193019ec9
Vishal Gupta <guptaaryan1103@gmail.com>
2026-02-01 22:43:09 +0530

    Connection Application from database

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 - Create an App
 - Install these depandancies like express, mongoose, nodemon, jsonwebtoken, bcrypt, cookie-parser, dotenv and cors
 - Write the middlewares
+- Create a new project on mongoDB community server atlas
+- Connect database from your applications
--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/database");
+dotenv.config({});
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 // middlewares
 app.use(express.json());
@@ -18,6 +22,13 @@ app.get("/", (req, res) => {
   return res.status(200).json({ message: "I am from backend", success: true });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is sucessfully running at ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    console.log("Database Connection is established!");
+    app.listen(PORT, () => {
+      console.log(`Server is sucessfully running at ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Database connection is not established!`);
+  });
