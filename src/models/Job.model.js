4ba8bfaa4feb60b535cfdf355d0199569c032513
Vishal Gupta <guptaaryan1103@gmail.com>
2026-02-02 10:53:09 +0530

    Create a Job model for Database

---
+++ src/models/Job.model.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    requied: true,
+    trim: true,
+    validate: {
+      validator: (value) =>
+        typeof value === "string" && value.trim().length > 0,
+      message: "Invalid Input String",
+    },
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 10,
+    maxlength: 500,
+    validate: {
+      validator: (value) =>
+        typeof value === "string" && value.trim().length > 0,
+      message: "Invalid Input String",
+    },
+  },
+  requirement: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: (value) =>
+        typeof value === "string" && value.trim().length > 0,
+      message: "Invalid Input String",
+    },
+  },
+  salary: {
+    type: Number,
+    require: true,
+    validate: {
+      validator: (value) => Number.isFinite(value),
+      message: (props) => `${props.value} is not a valid number`,
+    },
+  },
+  location: {
+    type: String,
+    require: true,
+    validate: {
+      validator: (value) =>
+        typeof value === "string" && value.trim().length > 0,
+      message: "Invalid Input String",
+    },
+  },
+  jobType: {
+    type: String,
+    require: true,
+    validate: {
+      validator: (value) =>
+        typeof value === "string" && value.trim().length > 0,
+      message: "Invalid Input String",
+    },
+  },
+  positions: {
+    type: Number,
+    requied: true,
+    validate: {
+      validator: (value) => Number.isFinite(value),
+      message: (props) => `${props.value} is not a valid number`,
+    },
+  },
+  company: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Company",
+    required: true,
+  },
+  created_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  applications: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Application",
+    required: true,
+  },
+});
+
+const Job = mongoose.model("Job", JobSchema);
+module.exports = Job;
